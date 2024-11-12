479e986ff6843e1e2b75ecd7b9c38480f6c38e30
yusraimran2 <yusraimran12@hotmail.com>
2024-11-12 21:22:18 +0500

    Add files via upload

---
+++ Q21.js
@@ -0,0 +1,9 @@
+"use strict";
+//  21:They think of something you could store in a Typescript object.Write a program that creates objects containing these items.
+let obj = {
+    name: "Yusra Imran",
+    age: 17,
+    salary: "20000",
+    No: 7
+};
+console.log(obj);
